c899ba2ce4c33cd164810778703a1365380f97a5
David Härdeman <david@hardeman.nu>
2025-09-24 14:53:54 +0200

    dhcpv4: rename handle_addrlist_change
    Rename handle_addrlist_change() to dhcpv4_addrlist_change() for more
    consistency with the other function names.
    
    Signed-off-by: David Härdeman <david@hardeman.nu>

--- src/dhcpv4.c
+++ src/dhcpv4.c
@@ -1141,7 +1141,7 @@ out:
 	return ret;
 }
 
-static void handle_addrlist_change(struct interface *iface)
+static void dhcpv4_addrlist_change(struct interface *iface)
 {
 	struct odhcpd_ipaddr ip;
 	struct odhcpd_ref_ip *a;
@@ -1194,7 +1194,7 @@ static void dhcpv4_netevent_cb(unsigned long event, struct netevent_handler_info
 		dhcpv4_setup_interface(iface, true);
 		break;
 	case NETEV_ADDRLIST_CHANGE:
-		handle_addrlist_change(iface);
+		dhcpv4_addrlist_change(iface);
 		break;
 	default:
 		break;
