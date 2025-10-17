054ddd3a6bfd213d4ea1c39a31eea9e6180e5f7c
David Härdeman <david@hardeman.nu>
2025-10-17 13:59:37 +0200

    dhcpv4: define val where it is used in dhcpv4_setup_interface()
    This makes it a tiny bit easier to see immediately what "val" is.
    
    Signed-off-by: David Härdeman <david@hardeman.nu>

--- src/dhcpv4.c
+++ src/dhcpv4.c
@@ -1300,7 +1300,7 @@ int dhcpv4_setup_interface(struct interface *iface, bool enable)
 		.sin_port = htons(DHCPV4_SERVER_PORT),
 		.sin_addr = { INADDR_ANY },
 	};
-	int val = 1;
+	int val;
 	int fd;
 
 	if (iface->dhcpv4_event.uloop.fd >= 0) {
@@ -1322,7 +1322,7 @@ int dhcpv4_setup_interface(struct interface *iface, bool enable)
 		goto err;
 	}
 
-	/* Basic IPv4 configuration */
+	val = 1;
 	if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val)) < 0) {
 		error("setsockopt(SO_REUSEADDR): %m");
 		goto err;
