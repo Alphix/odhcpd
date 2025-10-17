6499f92c9e6f1cc6d61e019c8f1cf89bf75a0c01
David Härdeman <david@hardeman.nu>
2025-10-17 13:57:14 +0200

    dhcpv4: simplify error handling in dhcpv4_setup_addresses()
    Remove the "ret" variable which can anyway always just be -1 on error, and
    rename the "out" label to "err" to clarify that it's only used on error.
    
    Signed-off-by: David Härdeman <david@hardeman.nu>

--- src/dhcpv4.c
+++ src/dhcpv4.c
@@ -1295,7 +1295,6 @@ static int dhcpv4_setup_addresses(struct interface *iface)
 
 int dhcpv4_setup_interface(struct interface *iface, bool enable)
 {
-	int ret = 0;
 	struct sockaddr_in bind_addr = {
 		.sin_family = AF_INET,
 		.sin_port = htons(DHCPV4_SERVER_PORT),
@@ -1320,69 +1319,59 @@ int dhcpv4_setup_interface(struct interface *iface, bool enable)
 	fd = socket(AF_INET, SOCK_DGRAM | SOCK_CLOEXEC, IPPROTO_UDP);
 	if (fd < 0) {
 		error("socket(AF_INET): %m");
-		ret = -1;
-		goto out;
+		goto err;
 	}
 
 	/* Basic IPv4 configuration */
 	if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val)) < 0) {
 		error("setsockopt(SO_REUSEADDR): %m");
-		ret = -1;
-		goto out;
+		goto err;
 	}
 
 	if (setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &val, sizeof(val)) < 0) {
 		error("setsockopt(SO_BROADCAST): %m");
-		ret = -1;
-		goto out;
+		goto err;
 	}
 
 	if (setsockopt(fd, IPPROTO_IP, IP_PKTINFO, &val, sizeof(val)) < 0) {
 		error("setsockopt(IP_PKTINFO): %m");
-		ret = -1;
-		goto out;
+		goto err;
 	}
 
 	val = IPTOS_CLASS_CS6;
 	if (setsockopt(fd, IPPROTO_IP, IP_TOS, &val, sizeof(val)) < 0) {
 		error("setsockopt(IP_TOS): %m");
-		ret = -1;
-		goto out;
+		goto err;
 	}
 
 	val = IP_PMTUDISC_DONT;
 	if (setsockopt(fd, IPPROTO_IP, IP_MTU_DISCOVER, &val, sizeof(val)) < 0) {
 		error("setsockopt(IP_MTU_DISCOVER): %m");
-		ret = -1;
-		goto out;
+		goto err;
 	}
 
 	if (setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, iface->ifname,
 		       strlen(iface->ifname)) < 0) {
 		error("setsockopt(SO_BINDTODEVICE): %m");
-		ret = -1;
-		goto out;
+		goto err;
 	}
 
 	if (bind(fd, (struct sockaddr *)&bind_addr, sizeof(bind_addr)) < 0) {
 		error("bind(): %m");
-		ret = -1;
-		goto out;
+		goto err;
 	}
 
-	if (dhcpv4_setup_addresses(iface) < 0) {
-		ret = -1;
-		goto out;
-	}
+	if (dhcpv4_setup_addresses(iface) < 0)
+		goto err;
 
 	iface->dhcpv4_event.uloop.fd = fd;
 	iface->dhcpv4_event.handle_dgram = dhcpv4_handle_dgram;
 	odhcpd_register(&iface->dhcpv4_event);
 	return 0;
 
-out:
+err:
 	close(fd);
-	return ret;
+	return -1;
 }
 
 static void dhcpv4_addrlist_change(struct interface *iface)
